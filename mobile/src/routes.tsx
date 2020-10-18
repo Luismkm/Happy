8f5fac3036b346dc9235978907bd1a799376eab0
Luis Moraes <luismarcelo13@yahoo.com.br>
2020-10-17 23:03:31 -0300

    add mobile

---
+++ mobile/src/routes.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+const { Navigator, Screen } = createStackNavigator()
+
+import Header from './components/Header'
+
+import OrphanagesMap from './pages/OrphanagesMap'
+import OrphanagesDetails from './pages/OrphanagesDetails'
+
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition'
+import OrphanageData from './pages/CreateOrphanage/OrphanageData'
+
+
+export default function Routes() {
+  return(
+    <NavigationContainer>
+      <Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: '#f2f3f5' } }}>
+        <Screen name="OrphanagesMap" component={OrphanagesMap} />
+       
+        <Screen 
+          name="OrphanagesDetails" 
+          component={OrphanagesDetails}
+          options={{
+            headerShown: true,
+            header: () => <Header showCancel={false} title="Orfanato" />
+          }} 
+        />
+
+        <Screen 
+          name="SelectMapPosition" 
+          component={SelectMapPosition}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Selecione no mapa" />
+          }}  
+        />
+
+        <Screen 
+          name="orphanageData" 
+          component={OrphanageData}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Informe os dados" />
+          }}  
+        />
+      </Navigator>
+    </NavigationContainer>
+  )
+}
